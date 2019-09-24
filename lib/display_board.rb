8bb61438502ca0a4f2adb597c40db10f54fe5b9a
scrappy-system-3850 <aevient@gmail.com>
2019-09-24 21:30:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ def display_board
   puts "#{board[4]}|#{board[5]}| #{board[6]}"
   puts "-----------"
   puts "#{board[7]}|#{board[8]}| #{board[9]}"
-end 
+end
 
 board = ["X","X","X","X","X","X","X","X","X"]
 # out the current state.
