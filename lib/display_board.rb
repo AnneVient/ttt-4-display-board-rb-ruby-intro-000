fbc420d38851286df106e94e8fc3ace5731bd8f7
scrappy-system-3850 <aevient@gmail.com>
2019-09-24 21:33:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
-def display_board
-  puts "#{board[0]}|#{board[1]}| #{board[3]}"
+def display_board(board)
+  puts " #{board[0]} |#{board[1]} | #{board[3]} "
   puts "-----------"
-  puts "#{board[4]}|#{board[5]}| #{board[6]}"
+  puts " #{board[4]} |#{board[5]} | #{board[6]} "
   puts "-----------"
-  puts "#{board[7]}|#{board[8]}| #{board[9]}"
+  puts " #{board[7]} |#{board[8]} | #{board[9]} "
 end
 
 board = ["X","X","X","X","X","X","X","X","X"]
