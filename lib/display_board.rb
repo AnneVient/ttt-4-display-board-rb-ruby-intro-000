6f2517c568c861425b7d409a457acc519470472e
scrappy-system-3850 <aevient@gmail.com>
2019-09-24 21:29:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-# Define display_board that accepts a board and prints
+def display_board
+  puts "#{board[0]}|#{board[1]}| #{board[3]}"
+  puts "-----------"
+  puts "#{board[4]}|#{board[5]}| #{board[6]}"
+  puts "-----------"
+  puts "#{board[7]}|#{board[8]}| #{board[9]}"
+end 
+
+board = ["X"," "," "," ","X"," "," "," ","X"]
 # out the current state.
